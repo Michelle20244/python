51f5ac59fa24ab1647254af8aa54f1979cded95e
Michelle20244 <183660373+Michelle20244@users.noreply.github.com>
2026-09-10 15:04:10 -0500

    Add files via upload

---
+++ loops.py
@@ -0,0 +1,25 @@
+"""
+-----------------------------------------------------------------------
+ASSIGNMENT REQUIREMENTS
+-----------------------------------------------------------------------
+[ ] 1. Header Docstring included.
+[ ] 2. Task 1: While Loop (The Nagging Kid)
+       - Repeats "Are we there yet?" until user types "yes".
+       - Uses a boolean variable to control the loop.
+[ ] 3. Task 2: For Loop (99 Bottles of Beer)
+       - Counts backwards from 99 to 1.
+       - Prints "[number] bottles of beer on the wall!"
+[ ] 4. Upload to GitHub and paste the link below.
+-----------------------------------------------------------------------
+"""
+
+are_we_there = False
+
+while are_we_there == False:
+    answer = input("Are we there yet? ")
+
+    if answer.strip().lower() == "yes":
+        are_we_there = True
+
+for bottles in range(99, 0, -1):
+    print(f"{bottles} bottles of beer on the wall!")
